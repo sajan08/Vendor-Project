614e535b245a22a255c3c4d3669d7ccfe24df15d
sajan sanjay borle <90256116+sajan08@users.noreply.github.com>
2023-12-08 15:23:25 +0530

    Add files via upload

---
+++ vendor_management/vendors/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Vendor
+from .models import PurchaseOrder
+
+class VendorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vendor
+        fields = ['id', 'vendor_code', 'name', 'contact_details', 'address']
+        
+
+class PurchaseOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PurchaseOrder
+        fields = ['id', 'vendor', 'po_number', 'order_date', 'items', 'quantity', 'status']
